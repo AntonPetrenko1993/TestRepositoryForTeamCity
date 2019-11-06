56288c2684dec21ba037040d9001c110ecb2aebc
Anton Petrenko <anton.petrenko@ua.akvelon.com>
2019-11-06 16:27:38 +0200

    Add project files.

---
+++ CalculatorTestProject.Tests/CalculatorTest.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorTestProject.Tests
+{
+    [TestFixture]
+    class CalculatorTest
+    {
+        private Calculator calculator;
+
+        [SetUp]
+        public void Init()
+        {
+            calculator = new Calculator();
+        }
+
+        [Test]
+        public void Sum_2_Plus_2_returned_4()
+        {
+            int expectedResult = 4;
+            int actualResult = calculator.Sum(2, 2);
+
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [Test]
+        public void Sub_4_Minus_2_returned_2()
+        {
+            int expectedResult = 2;
+            int actualResult = calculator.Sub(4, 2);
+
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [Test]
+        public void Mul_2_Multiply_2_returned_4()
+        {
+            int expectedResult = 4;
+            int actualResult = calculator.Mul(2, 2);
+
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+
+        [Test]
+        public void Div_4_Divide_2_returned_2()
+        {
+            int expectedResult = 2;
+            int actualResult = calculator.Div(4, 2);
+
+            Assert.AreEqual(actualResult, expectedResult);
+        }
+    }
+}
---
+++ CalculatorTestProject/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculatorTestProject
+{
+    public class Calculator
+    {
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+        public int Div(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
